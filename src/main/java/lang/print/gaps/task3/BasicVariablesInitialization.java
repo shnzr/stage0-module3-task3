1c5b6d9c16ff36adfaaed7a7a2e00d067f310cfa
Olim <shnzrvolim@gmail.com>
2022-09-24 05:00:49 +0500

    made declaration and initialization separate in task1

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -5,7 +5,9 @@ public class BasicVariablesInitialization {
         int first;
         int second;
         int third;
-        first = 1; second = 10; third = 100;
+        first = 1;
+        second = 10;
+        third = 100;
         System.out.println(first + "\n" + second + "\n" + third);
     }
 }
